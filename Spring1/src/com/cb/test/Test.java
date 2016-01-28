4de9ac0e3b6e80041479c30b404518d5add26ca8
chenbin <chenbkeep@163.com>
2016-01-28 23:29:22 +0800

    CSystem的UI

--- Spring1/src/com/cb/test/Test.java
+++ Spring1/src/com/cb/test/Test.java
@@ -5,13 +5,15 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.stereotype.Service;
 
 import com.cb.service.UserService;
+import com.cb.util.ApplicaionContextUtil;
 
 public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+		//ApplicationContext applicationContext=new ClassPathXmlApplicationContext("applicationContext.xml");
+		ApplicationContext applicationContext=ApplicaionContextUtil.getApplicationContext();
 		UserService userService=(UserService)applicationContext.getBean("userService");
 		userService.sayHello();
 	}
